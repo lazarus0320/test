033b3703a6aab4df5c81777efe9fda7d65d6febf
dgjinsu <rlawlstn3206@naver.com>
2023-02-23 19:04:39 +0900

    port 8080 Change

--- backend/src/main/java/com/example/demo/controller/TestController.java
+++ backend/src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(originPatterns = "http://localhost:8080")
 @RestController
 public class TestController {
     @GetMapping("/test")
